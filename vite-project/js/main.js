37e8c134202780e56969bdd7a34eb3e2e445b489
mr. whalen <michael.whalen@sitechhs.com>
2021-11-18 10:08:48 -0500

    asd

---
+++ vite-project/js/main.js
@@ -0,0 +1,6 @@
+import "../styles/style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>I hear you Cindy</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
